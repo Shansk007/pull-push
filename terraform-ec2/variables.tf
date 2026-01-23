e4e2ef1d609b26424aef748a7b2fb90793c2835a
Shansk007 <91014797+Shansk007@users.noreply.github.com>
2026-01-23 12:38:50 +0530

    tf update

--- terraform-ec2/variables.tf
+++ terraform-ec2/variables.tf
@@ -3,12 +3,12 @@ variable "aws_region" {
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "ami_id" {
   description = "Amazon Linux 2 AMI"
-  default     = "ami-0f5ee92e2d63afc18" # ap-south-1 (Mumbai)
+  default     = "ami-019715e0d74f695be" # ap-south-1 (Mumbai)
 }
 
 variable "key_name" {
